ed9920b3f38dd9392be190d82b75f42137ce3531
Dan Speers <dspeers@DS-CB-MacBook.local>
2018-06-07 10:49:12 -0400

    More awesome

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	title := "Jenkins X golang http example"
+	title := "Jenkins X golang http example - more awesome"
 
 	from := ""
 	if r.URL != nil {
